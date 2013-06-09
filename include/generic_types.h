559eb7be15a0e1153eb21f58c8007d70a366cf20
greenbender <byron.platt@gmail.com>
2013-06-09 02:57:29 -0300

    GPL3 licensed

--- include/generic_types.h
+++ include/generic_types.h
@@ -1,8 +1,23 @@
 /*
+Generic types for use in loom (or else where).
+Copyright (C) 2013 Commonwealth of Australia
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef __GENERIC_H__
-#define __GENERIC_H__
+#ifndef __GENERIC_TYPES_H__
+#define __GENERIC_TYPES_H__
 
 #include <talloc.h>
 #include <stdint.h>
--- tests/testrunner.c
+++ tests/testrunner.c
@@ -1,4 +1,23 @@
-/* testrunner.c */
+/*
+Unit testing code for loom. This is a generic novaprova testrunner.
+Copyright (C) 2013 Commonwealth of Australia
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Derived from NovaProva testrunner.c sample <http://www.novaprova.org>
+*/
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -10,8 +29,7 @@ int usage(char *prog) {
     return -1;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int result, c;
 
     np_runner_t *runner = np_init();
